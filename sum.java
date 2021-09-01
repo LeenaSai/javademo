30570c6ead7ed7c44bc1a0cd5bd2f8d5ad40d0d4
LeenaSai <66858274+LeenaSai@users.noreply.github.com>
2021-09-02 00:01:25 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,15 +1,10 @@
-public class Sum {
-
+public class Sum 
+{
   public static void main(String[] args) {
     
     System.out.println("Enter two numbers");
-    int first = 10;
-    int second = 20;
-    
-    System.out.println(first + " " + second);
-
-    // add two numbers
-    int sum = first + second;
+    int first = 10,second = 20, sum;
+    sum = first + second;
     System.out.println("The sum is: " + sum);
   }
 }
